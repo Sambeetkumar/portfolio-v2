30e542d0f56ca1d0a4e53c4cdde002bc06b27dc2
sambeet kumar khatua <khatuasambeet31@gmail.com>
2023-05-08 00:42:22 +0530

    modified contact text

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,8 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        Let's connect and bring your ideas to life! Whether you have a question, a project proposal,
+        or simply want to say hello, I'm just a click away. I’ll try my best to get back to you!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
